493dbee65b4721638656ffb06a1a6412fac34975
TanishqBansal <bansaltanishq472@gmail.com>
2023-12-10 13:35:04 +0530

    Understanding For Loop

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -7,6 +7,13 @@
 
 function countVowels(str) {
     // Your code here
+    let count_vowels = 0; // Variable For Maintaing The Count Of Vowels
+for(let index = 0; index < str.length; index++) {
+  if(str[index] == 'a' || str[index] == 'e' || str[index] == 'i' || str[index] == 'o' || str[index] == 'u' || str[index] == 'A' || str[index] == 'E' || str[index] == 'I' || str[index] == 'O'|| str[index] == 'U') {
+    count_vowels++;  // If find some vowel in str then increase the count 
+  }
+}
+return count_vowels;
 }
 
 module.exports = countVowels;
