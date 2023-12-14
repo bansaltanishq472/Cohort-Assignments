c3627eb565f29613b7c5cdb35e8e5e53e4acfa60
TanishqBansal <bansaltanishq472@gmail.com>
2023-12-14 22:44:49 +0530

    Anagram Done

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,22 @@
 */
 
 function isAnagram(str1, str2) {
+  const n1 = str1.length;
+  const n2 = str2.length;
+  if (n1 != n2) {
+    return false;
+  }
 
+  let arr1 = [...str1.toLowerCase()];
+  let arr2 = [...str2.toLowerCase()];
+
+  arr1.sort();
+  arr2.sort();
+
+  for (let index = 0; index < arr1.length; index++) {
+    if (arr1[index] != arr2[index]) return false;
+  }
+  return true;
 }
 
 module.exports = isAnagram;
