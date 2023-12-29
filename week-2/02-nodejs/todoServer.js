2853ef1e263a3a77b218cc14d2315fe665b599c8
TanishqBansal <bansaltanishq472@gmail.com>
2023-12-29 14:14:34 +0530

    Week 2 nodejs Done

--- week-2/02-nodejs/todoServer.js
+++ week-2/02-nodejs/todoServer.js
@@ -46,4 +46,62 @@
   
   app.use(bodyParser.json());
   
+
+  // Methood 1 -> Store in the Array todos and from there we manage every other thing. 
+let todos = [];
+app.get('/todos', function (req, res) {
+  res.json(todos); 
+});
+
+app.get('/todos/:id', function (req, res) {
+  const todo = todos.find(t => t.id === parseInt(req.params.id));
+  if (!todo) {
+    res.status(404).send();
+  }
+  else {
+    res.json(todo);
+  }
+});
+
+app.post('/todos', function (req, res) {
+  const newTodo = {
+    id: Math.floor(Math.random() * 1000000),
+    title: req.body.title,
+    description: req.body.description
+  }; 
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+});
+
+app.put('/todos/:id', function (req, res) {
+  const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id));
+  if (todoIndex === -1) {
+    res.status(404).send();
+  }
+  else {
+    todos[todoIndex].title = req.body.title;
+    todos[todoIndex].description = req.body.description;
+    res.json(todos[todoIndex]);
+  }
+});
+
+app.delete('/todos/:id', function (req, res) {
+  const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id));
+  if (todoIndex === -1) {
+    res.status(404).send();
+  }
+  else {
+    todos.splice(todoIndex, 1);
+    res.status(200).send();
+  }
+});
+
+app.use(function(req, res, next) {
+  res.status(404).send();
+});
+
+app.all('*', function (req, res) {
+  res.status(404).send();
+});
+
   module.exports = app;
